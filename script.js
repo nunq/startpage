5f3a065e81d6b124ebe09799b5c94e6ee74d965d
hyphenc <46054695+hyphenc@users.noreply.github.com>
2022-05-04 18:24:08 +0200

    adjust intervals

--- script.js
+++ script.js
@@ -79,6 +79,6 @@ updateImage();
 setDate();
 updateClock();
 getWeather();
-setInterval(updateClock, 1000);
-setInterval(updateImage, 60000);
-setInterval(getWeather, 600000);
+setInterval(updateClock, 1000); //1sec
+setInterval(updateImage, 180000); //3min
+setInterval(getWeather, 900000); //15 min
